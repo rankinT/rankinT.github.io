aa860a849741d2ff292f02187792d4fba23556dd
Rankin <rat81@pitt.edu>
2023-01-26 00:31:53 -0500

    finished redesign

--- scripts/index.js
+++ scripts/index.js
@@ -7,8 +7,7 @@ function reveal(){
     for (let i =0; i< reveals.length;i++){
         let windowHeight = window.innerHeight; //viewport height
         let elementTop = reveals[i].getBoundingClientRect().top; //height from top element to VP
-        let elementVisible = 3; //distance to from elm to top... to reveal
-
+        let elementVisible = 1; //distance to from elm to top... to reveal
 
         //add 'attr' class active to revealed and hidden
         if (elementTop < windowHeight - elementVisible) {
@@ -17,5 +16,22 @@ function reveal(){
         reveals[i].classList.remove("active");
         }
     }
+    
 }
 window.addEventListener("scroll",reveal);
+window.addEventListener("scroll",scrollPrompt)
+
+function scrollPrompt(){
+    
+    let prompt = document.getElementById('scroll-prompt');
+    if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight - 2) {
+        prompt.style.display = 'none';
+    }
+    else{
+        prompt.style.display = 'block';   
+    }
+}
+
+window.onscroll = function(ev) {
+    
+};
